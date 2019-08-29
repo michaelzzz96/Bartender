f104865d2311fa4ee721fd6660a5632a49a0e083
Michael Zulfekar <n00929403@ospreys.unf.edu>
2019-08-28 21:57:31 -0400

    Importing Project

---
+++ Bartender/Models/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace Bartender.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        [Required]
+        [Display(Name = "Beverage")]
+        public CocktailName CocktailName { get; set; }
+
+        [Required]
+        [Display(Name = "Ordered By")]
+        public string OrderedBy { get; set; }
+    }
+
+
+    public enum CocktailName
+    {
+        [Display(Name = "Beer")] Beer = 1,
+        [Display(Name = "Wine")] Wine,
+        [Display(Name = "Spirit")] Spirita,
+        [Display(Name = "Whisky")] Whisky,
+       
+    }
+}
